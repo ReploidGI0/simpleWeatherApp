1f0f4ce7ece3fb3c4f7753b2dac38c6c373502ee
ReploidGI0 <gilespericueta@gmail.com>
2025-05-12 19:03:50 -0700

    Prueba de comunicacion con API Weather

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,20 @@
+import axios from "axios"
+import type { SearchType } from "../types"
+
 export default function useWeather() {
-    const fetchWeather = () => {
-        console.log('Consultando Clima...')
+    const fetchWeather = async (search: SearchType) => {
+
+        const appid = import.meta.env.VITE_API_KEY
+
+        try{
+            const geoUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.country}&appid=${appid}`
+
+            const {data} = await axios(geoUrl)
+            console.log(data)
+
+        } catch (error){
+            console.log(error)
+        }
     }
 
     return{
